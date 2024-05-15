a2574244ea7f60746909b39c8db4d65230af70eb
Mohamed Amine Chennadi <mochenna@e1r7p2.1337.ma>
2024-05-15 23:38:26 +0100

    parcin mazain

--- philo/check_utils.c
+++ philo/check_utils.c
@@ -6,8 +6,49 @@
 /*   By: mochenna <mochenna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 17:19:27 by mochenna          #+#    #+#             */
-/*   Updated: 2024/05/14 17:19:52 by mochenna         ###   ########.fr       */
+/*   Updated: 2024/05/15 23:31:58 by mochenna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
+void more_check(char *s)
+{
+    int i;
+    
+    i = 0;
+    while (s[i] == ' ')
+        i++;
+    if (!s[i])
+        ft_perror("OPS! you have  invalid argument [enter just number]\n");
+    else if (s[i] == '-')
+        ft_perror("OPS! you have  invalid argument [the number can not be negative ]\n");
+    else if ((s[i] == '+' && (!s[i + 1] || !(s[i + 1] >= '0' && s[i + 1] <= '9'))))
+        ft_perror("OPS! you have  invalid argument [enter just valid number]\n");
+    else if (s[i] == '+')
+        i++;
+    while (s[i])
+    {
+        if (!(s[i] >= '0' && s[i] <= '9'))
+                ft_perror("OPS! you have  invalid argument [enter just number]\n");
+        i++;
+    }
+}
+void check(int ac, char **av)
+{
+    int i;
+    long r;
+
+    if (ac != 5 && ac != 6)
+        ft_perror("OPS! you have  invalid argument\n");
+    i = 1;
+    while (i < ac)
+    {
+        if (!av[i][0])
+            ft_perror("OPS! you have  invalid argument [ the string is emty ]\n");
+        more_check(av[i]);
+        r = to_int(av[i]);
+        if (r < MININT || r > MAXINT)
+            ft_perror("OPS! you have  invalid argument [you have agument geart max or less min int ]\n");
+        i++;
+    }
+}
--- philo/philosopher.h
+++ philo/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: mochenna <mochenna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 12:03:21 by mochenna          #+#    #+#             */
-/*   Updated: 2024/05/14 17:41:29 by mochenna         ###   ########.fr       */
+/*   Updated: 2024/05/15 23:02:56 by mochenna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@
 void printstr(char *s);
 long	to_int(char *str);
 void ft_perror(char *s);
+void check(int ac, char **av);
 
 #endif
